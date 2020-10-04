9cd780d2639b080b6544cfd11f2415b27b2d906c
ujjwalschauhan <66547370+ujjwalschauhan@users.noreply.github.com>
2020-10-04 10:33:12 +0530

    added sound extension using requests

---
+++ sounds.py
@@ -0,0 +1,17 @@
+from tkinter import *
+import pygame
+root = Tk()
+root.title('codemy.com')
+root.iconbitmap('c:/gui/codemy.ico')
+root.geometry("500x400")
+pygame.mixer.init()
+
+def play():
+	pygame.mixer.music.load("C:/gui/audio/Not_Me.mp3")
+	pygame.mixer.music.play(loops=0)
+
+my_button = Button(root, text='Play Song', font=("Helvetica", 32), command=play)
+my_button.pack(pady=20)
+
+
+root.mainloop()
